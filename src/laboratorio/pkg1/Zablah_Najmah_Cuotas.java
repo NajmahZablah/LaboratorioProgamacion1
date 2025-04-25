5dbf7e19869e49eb32e8b8306f608e6a94dde8c5
najmah-zablah <najmahzablah@gmail.com>
2025-04-25 16:00:16 -0600

    anexo ejercicio

---
+++ src/laboratorio/pkg1/Zablah_Najmah_Cuotas.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author najma
+ */
+public class Zablah_Najmah_Cuotas {
+     public static void main(String[] args) {
+         //Blah blah blah blah...
+         Scanner scanner=new Scanner(System.in);
+         
+         System.out.print("Ingresar el monto del prestamo: HNL.");
+         double MontoPrestamo=scanner.nextDouble();
+         
+         System.out.print("Ingresar el plazo en meses: ");
+         int PlazoMensual=scanner.nextInt();
+         
+         System.out.print("Ingresar la tasa de interes mensual: %");
+         int TasaInteresMensual=scanner.nextInt();
+          double PorcentajeDecimalTIM=(TasaInteresMensual/100);
+         
+         System.out.print("Ingresar comision fija por cuota: HNL.");
+         double ComisionFija=scanner.nextDouble();
+         
+         System.out.print("Ingresar el porcentaje del seguro de la cuota: %");
+         int SeguroCuota=scanner.nextInt();
+         double PorcentajeSC=(SeguroCuota/100);
+         
+         double InteresTotal=(MontoPrestamo*PlazoMensual*PorcentajeDecimalTIM);
+         double MontoTotalBase=(MontoPrestamo+InteresTotal);
+         double CuotaBase=(MontoTotalBase/PlazoMensual);
+         double Seguro=(CuotaBase*PorcentajeSC);
+         
+         double CuotaTotal=(CuotaBase+ComisionFija+Seguro);
+         double TotalPagar=(CuotaTotal*PlazoMensual);
+         
+         //Salida
+         System.out.println("\n****Cuotas Mensuales****");
+         System.out.println("Cuota de Pago Mensual: HNL."+CuotaTotal);
+         System.out.println("Total a Pagar: HNL."+TotalPagar);
+     }
+}
--- src/laboratorio/pkg1/Zablah_Najmah_PlanilladeEmpleado.java
+++ src/laboratorio/pkg1/Zablah_Najmah_PlanilladeEmpleado.java
@@ -4,10 +4,35 @@
  */
 package laboratorio.pkg1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author najma
  */
 public class Zablah_Najmah_PlanilladeEmpleado {
-    
+     public static void main(String[] args) {
+         
+         //Las horas laboradas al mes y precio por hora deben ser asignadas por el usuario
+         Scanner scanner=new Scanner(System.in).useDelimiter("\\n");
+         
+         System.out.print("Ingresar el nombre del empleado: ");
+         String NombreEmpleado=scanner.next();
+         
+         System.out.print("Ingresar las horas mensuales trabajadas: ");
+         int HoraMensual=scanner.nextInt();
+         
+         System.out.print("Ingresar la tarifa por hora: ");
+         double TarifaHora=scanner.nextDouble();
+         
+         double SalarioSemanal=(HoraMensual*TarifaHora)/4;
+         
+         //Salida
+         System.out.println("\n****Boleta del Empleado****");
+         System.out.println("Nombre del empleado: "+NombreEmpleado);
+         System.out.println("Hora de Trabajo Mensual: "+HoraMensual);
+         System.out.println("Tarifa por Hora: Lps."+TarifaHora);
+         System.out.println("Salario del Empleado Semanal: Lps."+SalarioSemanal);
+          
+     }      
 }
